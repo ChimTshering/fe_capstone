9fbed6c2450428055cf54db40536c710e44e3c96
chimi <chimitshering505@gmail.com>
2023-03-23 13:21:30 +0600

    set header meta data

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <header>
+      <meta name="description" content="little lemon restaurant app" />
+      <meta name="og:title" content=" Little Lemon" />
+      <meta name="og:description" content=" online restaurant booking app" />
+      <meta name="og:image" content="../../public/logo.png" />
+    </header>
+  );
+};
